c9d3fcd99d17d552aa3a55f9de3100d300ba1e16
NCM <nmitchell34@gatech.edu>
2020-08-06 20:32:46 -0400

    removed hash routing to see if it works with just homnepage line

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Contact from "../src/views/contact";
 import Portfolio from "../src/views/portfolio";
 import Index from "../src/views/index";
@@ -7,19 +7,13 @@ import Index from "../src/views/index";
 
 function App() {
   return (
-    <HashRouter basename='/'>
+    <Router>
       <div>
-        <ul>
-          <li><Link to="/">Index</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/portfolio">Portfolio</Link></li>
-        </ul>
-        <hr />
         <Route exact path="/" component={Index} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/portfolio" component={Portfolio} />
       </div>
-    </HashRouter>
+    </Router>
   );
 }
 
