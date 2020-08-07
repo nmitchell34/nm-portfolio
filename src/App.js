82dc355201d2bfa7c55526ee9bd82323ba44b8e2
NCM <nmitchell34@gatech.edu>
2020-08-06 20:28:08 -0400

    trying hash routing for gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 import Contact from "../src/views/contact";
 import Portfolio from "../src/views/portfolio";
 import Index from "../src/views/index";
@@ -7,8 +7,14 @@ import Index from "../src/views/index";
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename='/'>
       <div>
+        <ul>
+          <li><Link to="/">Index</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/portfolio">Portfolio</Link></li>
+        </ul>
+        <hr />
         <Route exact path="/" component={Index} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/portfolio" component={Portfolio} />
