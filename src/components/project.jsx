37c6e29308d568ed0c6b20487255fb6c7832ce4f
NCM <nmitchell34@gatech.edu>
2020-08-06 23:39:34 -0400

    added array of info for projects component

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,6 +1,47 @@
 import React from "react";
 
 const project = () => {
+  const [projects, setProjects] = useState({
+    proj: [
+      {
+        imgSRC: "../../public/Images/EntertaiMeMain.png",
+        deployedHREF: "https://mayastucky.github.io/gt-project-1/",
+        appName: "Entertain Me App",
+        repoHREF: "https://github.com/nmitchell34/gt-project-1",
+      },
+      {
+        imgSRC: "../../public/Images/2020-06-22 (4).png",
+        deployedHREF: "https://mayastucky.github.io/gt-project-1/",
+        appName: "Entertain Me App",
+        repoHREF: "https://github.com/nmitchell34/gt-project-1",
+      },
+      {
+        imgSRC: "../../public/Images/2020-06-22 (6).png",
+        deployedHREF: "https://nmitchell34.github.io/Bootcamp_HW5/",
+        appName: "Day Planner",
+        repoHREF: "https://github.com/nmitchell34/Bootcamp_HW5",
+      },
+      {
+        imgSRC: "../../public/Images/EntertaiMeMain.png",
+        deployedHREF: "https://mayastucky.github.io/gt-project-1/",
+        appName: "Entertain Me App",
+        repoHREF: "https://github.com/nmitchell34/gt-project-1",
+      },
+      {
+        imgSRC: "../../public/Images/employeeTracker.gif",
+        deployedHREF: "",
+        appName: "Employee Tracker",
+        repoHREF: "https://github.com/nmitchell34/Bootcamp_HW12_EmployeeTracker",
+      },
+      {
+        imgSRC: "../../public/Images/noteTakerImg.png",
+        deployedHREF: "https://bootcamp-hw11-gt-note-taker.herokuapp.com/",
+        appName: "Note Taker",
+        repoHREF: "https://github.com/nmitchell34/Bootcamp_HW11",
+      },
+    ],
+  });
+
   return (
     <div>
       <div className="thumbnail text-center">
