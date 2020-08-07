f8e1847ad9794629ab59fe434b440dcfd62ceb75
NCM <nmitchell34@gatech.edu>
2020-08-06 20:28:59 -0400

    now added hashrouting

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/portfolio" component={Portfolio} />
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
